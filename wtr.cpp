7552f91816f5c2ef3e687f9801e74b62090dcc4d
Anonmixs Amaro Afonso <anonmixs.afonso@gmail.com>
2025-11-15 22:59:43 -0300

    Updated: Add days of the week in CLI interface.

--- wtr.cpp
+++ wtr.cpp
@@ -11,9 +11,12 @@ void wtr_set ()
   {
     std::string file_path = "/home/anon/Study_Programs/Cpp/day_week_tasks/" + std::to_string(i) + ".txt";
     std::ofstream output_file (file_path);
+    std::cout << "*********************************\n";
+    std::cout << "Week day: " << i + 1 << "\n";
+    std::cout << "*********************************\n";
     for (int i = 0; i < 17; i++) 
     {
-      std::cout << "Type the task of " << hour_of_day << ": ";
+      std::cout << "Type the task of " << hour_of_day << ":00h: ";
       std::getline (std::cin, week_task);
     
       if (output_file.is_open())
